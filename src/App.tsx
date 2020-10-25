99681da01e5eef01fa830e763c1cba46ced4ffb7
Niv <nivian.lee@gmail.com>
2020-10-25 17:37:41 +0800

    Changed app icon, migrated toolbar and side drawer to App, changed colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import "./App.css";
 import TodoList from "./Container/TodoList";
-
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import Grid from "@material-ui/core/Grid";
 
 const themeLight = createMuiTheme({
   palette: {
@@ -13,8 +21,21 @@ const themeLight = createMuiTheme({
       // dark: will be calculated from palette.primary.main,
       // contrastText: will be calculated to contrast with palette.primary.main
     },
+    secondary: {
+      light: "#0066ff",
+      main: "#0044ff",
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: "#fff",
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
     background: {
-      default: "#fff5d7",
+      default: "#ffffff",
     },
   },
   overrides: {
@@ -27,13 +48,89 @@ const themeLight = createMuiTheme({
   },
 });
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    background: "#ffffff",
+  },
+  toolbarFont: {
+    color: "#000",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: "auto",
+  },
+  content2: {
+    display: "flex",
+    justifyContent: "flex-start",
+    width: "100%",
+    padding: theme.spacing(1),
+    marginTop: 64,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <MuiThemeProvider theme={themeLight}>
-        <CssBaseline />
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar} elevation={0}>
+        <Toolbar>
+          <Typography className={classes.toolbarFont} variant="h6" noWrap>
+            Clipped drawer
+          </Typography>
+        </Toolbar>
+        <Divider />
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {["All mail", "Trash", "Spam"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <TodoList />
-      </MuiThemeProvider>
+      </main>
     </div>
   );
 };
